b05793306535e84aee946024baa138e2a501b0b5
彬少 <dz303241@163.com>
2024-03-26 21:12:04 +0800

    Update index.tsx

--- src/pages/project/index.tsx
+++ src/pages/project/index.tsx
@@ -20,7 +20,7 @@ const DESCRIPTION = translate({
   message: '学而无用，不如学而用之。这里是我在技术领域中努力实践和应用的最佳证明。',
 })
 
-// const GITHUB_URL = 'https://github.com/kuizuo'
+// const GITHUB_URL = 'https://github.com/zuqiuxie'
 
 type ProjectState = {
   scrollTopPosition: number
