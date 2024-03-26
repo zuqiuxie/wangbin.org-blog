7c325886e781e406cf949d31ec8fa01ed66f7564
彬少 <dz303241@163.com>
2024-03-26 20:12:05 +0800

    更新

--- data/social.ts
+++ data/social.ts
@@ -18,14 +18,14 @@ type SocialValue = {
 }
 
 const social: Social = {
-  github: 'https://github.com/kuizuo',
-  twitter: 'https://twitter.com/kuizuo',
+  github: 'https://github.com/zuqiuxie',
+  twitter: 'https://twitter.com/zuqiuxie',
   juejin: 'https://juejin.cn/user/1565318510545901',
   qq: 'https://img.kuizuo.cn/qq.png',
   wx: 'https://img.kuizuo.cn/wechat.png',
   // zhihu: 'https://www.zhihu.com/people/kuizuo',
   // cloudmusic: 'https://music.163.com/#/user/home?id=1333010742',
-  email: 'mailto:hi@kuizuo.cn',
+  email: 'admin@wangbin.org',
   discord: 'https://discord.gg/M8cVcjDxkz',
 }
 
