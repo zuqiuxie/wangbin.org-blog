ca62081e6832a77975f14f720b82b1b1e1f38bc6
彬少 <dz303241@163.com>
2024-07-04 18:09:36 +0800

    Update features.tsx

--- data/features.tsx
+++ data/features.tsx
@@ -13,12 +13,11 @@ const FEATURES: FeatureItem[] = [
   {
     title: translate({
       id: 'homepage.feature.developer',
-      message: 'TypeScript 全栈工程师',
+      message: '医疗器械工程师',
     }),
     text: (
       <Translate>
-        作为一名 TypeScript 全栈工程师，秉着能用 TS 绝不用 JS
-        的原则，为项目提供类型安全的保障，提高代码质量和开发效率。
+        从业有16年，国内一线医疗器械厂家工程师。
       </Translate>
     ),
     Svg: WebDeveloperSvg,
@@ -26,11 +25,11 @@ const FEATURES: FeatureItem[] = [
   {
     title: translate({
       id: 'homepage.feature.spider',
-      message: '会点逆向 & 爬虫',
+      message: '跑步 & 运动',
     }),
     text: (
       <Translate>
-        作为一名曾学习与实践逆向工程两年半的开发者，对于逆向工程有着浓厚的兴趣，同时造就了超凡的阅读代码能力。没有看不懂的代码，只有不想看的代码。
+        2016年开始跑步，参加过N次10km，2次全马。
       </Translate>
     ),
     Svg: SpiderSvg,
@@ -38,11 +37,11 @@ const FEATURES: FeatureItem[] = [
   {
     title: translate({
       id: 'homepage.feature.enthusiast',
-      message: '开源爱好者',
+      message: '网站 & 看书 & 开源 & Github',
     }),
     text: (
       <Translate>
-        作为一名开源爱好者，积极参与开源社区，为开源项目贡献代码，希望有生之年能够构建出一个知名的开源项目。
+        喜欢电脑、22年接触到Github，一发不可收拾，虽然不是科班出身，摸索着算是看到了大门。
       </Translate>
     ),
     Svg: OpenSourceSvg,
